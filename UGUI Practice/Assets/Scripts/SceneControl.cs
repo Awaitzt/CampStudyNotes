fd97133b12ae9dd66cba24a7aa27e50315c67f6d
Awaitzt <32785725+Awaitzt@users.noreply.github.com>
2018-07-12 20:30:58 +0800

    Add files via upload

---
+++ UGUI Practice/Assets/Scripts/SceneControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneControl : MonoBehaviour {
+    public Animator _startButton;
+    public Animator _fourMenPlane;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Open()
+    {
+        _startButton.SetBool("Is True",true);
+        _fourMenPlane.enabled = true;
+        _fourMenPlane.SetBool("isTrue",false);
+    }
+    public void Close()
+    {
+        _startButton.SetBool("Is True",false);
+        _fourMenPlane.SetBool("isTrue",true);
+    }
+}
