42271378517152f53240b56e46d52879770d4e6e
Awaitzt <32785725+Awaitzt@users.noreply.github.com>
2018-07-13 21:18:21 +0800

    Add files via upload

---
+++ Racer Game Practice/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject _racer;
+    private Vector3 _distance;
+    private float _delta = 0.03f;
+    //private Vector3 _eulerAngle;
+    private float _y;
+    private float _dis;
+	// Use this for initialization
+	void Start () {
+        //transform.SetParent(_racer.transform);
+        _distance = transform.position - _racer.transform.position;
+        _dis = Mathf.Sqrt(Mathf.Pow(_distance.x, 2)+ Mathf.Pow(_distance.z, 2));
+        //_eulerAngle = _racer.transform.eulerAngles;
+        //Debug.Log(_eulerAngle.y);Transform
+        _y = _racer.transform.rotation.y;
+        Debug.Log(_y);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        _distance.z = _dis * Mathf.Cos(_racer.transform.rotation.y - _y);
+        _distance.x = _dis * Mathf.Sin(_racer.transform.rotation.y - _y);
+        transform.position = Vector3.Lerp(transform.position, _racer.transform.position + _distance, _delta);
+        transform.LookAt(_racer.transform);
+    }
+}
---
+++ Racer Game Practice/Assets/Scripts/RacerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacerController : MonoBehaviour {
+    private float _speed=0f;
+    private float _acceleration=5f;
+    private float _breakAcceleration = 5f;
+    public float _rotateSpeed=45f;
+    public float _maxSpeed=25f;
+    private float _backMaxSpeed = 15f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow)&&_speed!=0)
+            transform.Rotate(Vector3.down*_rotateSpeed*Time.deltaTime);
+        if (Input.GetKey(KeyCode.RightArrow) && _speed != 0)
+            transform.Rotate(Vector3.up * _rotateSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if(_speed<0)
+                _speed += _breakAcceleration * Time.deltaTime;
+            else if (_speed < _maxSpeed)
+                _speed += _acceleration * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if(_speed>0)
+                _speed-= _breakAcceleration * Time.deltaTime;
+            else if (_speed > -_maxSpeed)
+                _speed -= _acceleration * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) == false && Input.GetKey(KeyCode.DownArrow) == false&&_speed!=0)
+        {
+            if (_speed > 0)
+            {
+                if (_speed - _acceleration * Time.deltaTime >= 0)
+                    _speed -= _acceleration * Time.deltaTime;
+                else
+                    _speed = 0;
+            }
+            else
+            {
+                if (_speed + _acceleration * Time.deltaTime <= 0)
+                    _speed += _acceleration * Time.deltaTime;
+                else
+                    _speed = 0;
+            }
+        }
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+    }
+}
